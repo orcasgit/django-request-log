d90c3299e8c811c45ce33363add83746ee7aec96
Percy Perez <percyp3@gmail.com>
2013-01-22 15:14:38 -0800

    Adding setup.py and version

---
+++ setup.py
@@ -0,0 +1,27 @@
+from setuptools import setup
+
+setup(
+    name='orcas-log',
+    version=__import__('log').__version__,
+    description='Pluggable Django applications to record user login and logout events and URL views.',
+    long_description=open('README.rst').read(),
+    author='Oregon Center for Applied Science',
+    author_email='pperez@orcasinc.com',
+    url='https://github.com/orcasgit/orcas-log',
+    license='Apache',
+    packages=['log'],
+    include_package_data=True,
+    zip_safe=False,
+    classifiers=[
+        'Development Status :: 4 - Beta',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved ::Apache',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
+)
