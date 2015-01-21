485494af1e55827b10dc4f9615f0913626acf6b2
Brad Pitcher <bradpitcher@gmail.com>
2015-01-20 16:03:31 -0800

    fix crash on userless request

--- log/middleware.py
+++ log/middleware.py
@@ -7,7 +7,7 @@ class RequestLoggingMiddleware(object):
     user.
     """
     def process_response(self, request, response):
-        if response.status_code == 200 and \
+        if response.status_code == 200 and hasattr(request, 'user') and \
            request.user and not request.user.is_anonymous():
             RequestLog.objects.create_log(request)
         return response
