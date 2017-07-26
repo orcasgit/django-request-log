c9ead7e3e6b39654054a31c7e9d2d075981798e7
Brad Pitcher <bradpitcher@gmail.com>
2017-07-26 16:37:08 -0700

    search on USERNAME_FIELD

--- log/admin.py
+++ log/admin.py
@@ -1,4 +1,4 @@
-from django.contrib import admin
+from django.contrib import admin, auth
 
 from .models import Log, RequestLog
 
@@ -7,7 +7,10 @@ class LogAdmin(admin.ModelAdmin):
     list_display = ('user', 'session', 'varname', 'stamp', 'value')
     ordering = ('-stamp',)
     list_filter = ('user',)
-    search_fields = ('user__username', 'varname')
+    search_fields = (
+        'user__{}'.format(auth.get_user_model().USERNAME_FIELD),
+        'varname',
+    )
     save_on_top = True
 admin.site.register(Log, LogAdmin)
 
